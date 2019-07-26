f8e892ee2b4d0268639cac1b0427670c1ab5be6b
Michael Hackman <reedhackman@gmail.com>
2019-07-26 14:45:11 -0700

    httpoison/poison

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Agot.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:httpoison, "~> 1.4"},
       {:phoenix, "~> 1.4.9"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
@@ -40,6 +41,7 @@ defmodule Agot.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:poison, "~> 3.1"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
