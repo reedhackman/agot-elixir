707b172739d3137bb40bfd3182e3401f9fce6755
Michael Hackman <reedhackman@gmail.com>
2019-08-14 03:07:33 -0700

    fix

--- assets/js/pages/decks/components/DecksFull.js
+++ assets/js/pages/decks/components/DecksFull.js
@@ -10,7 +10,7 @@ export default class extends React.Component{
     }
   }
   componentDidMount() {
-    games = this.props.games
+    let games = this.props.games
     let decks = {}
     games.forEach((game) => {
       if(!(decks[game.winner_faction])){
